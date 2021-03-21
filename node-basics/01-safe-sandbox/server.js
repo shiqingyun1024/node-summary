9e7e99c5fe72b9c6a182c3b22eb3694fa3eefdf4
814657780 <18037818812@163.com>
2021-03-21 22:31:02 +0800

    “Node.js”

--- node-basics/01-safe-sandbox/server.js
+++ node-basics/01-safe-sandbox/server.js
@@ -1,3 +1,4 @@
+// 服务器之间是没有跨域的概念的，跨域是浏览器的安全策略
 const https = require('https');
 https.get('https://m.maoyan.com/ajax/moreClassicList?sortId=1&showType=3&limit=10&offset=30&optimus_uuid=A5518FF0AFEC11EAAB158D7AB0D05BBBD74C9789D9F649898982E6542C7DD4796&optimus_risk_level=71&optimus_code=10',(res)=>{
     console.log(res);
