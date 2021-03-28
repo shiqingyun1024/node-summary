e5ffe6fd8b0a5d099da223575bc5d077b5f5ce9f
814657780 <18037818812@163.com>
2021-03-28 21:51:53 +0800

    “Node.js”

--- node-basics/04-buildin-modules/02-querystring/app.js
+++ node-basics/04-buildin-modules/02-querystring/app.js
@@ -5,9 +5,13 @@ const query = 'id=2&name=tongyi&from=北京'
 const query2 = 'id:2/name:tongyi/from:北京'
 // const queryEscape = 'id=2&name=tongyi&from=北京'
 const queryObj = {id:'2',name:'tongyi',from:'北京'}
+// querystring.stringify 对象转化为字符串
 const newQuery = querystring.stringify(queryObj,null,null,{
     encodeURIComponent(string){
         return querystring.unescape(string )
     }
 })
+// querystring.parse 字符串转化为对象
+const newParse = querystring.parse(query)
 console.log(newQuery);
+console.log(newParse);
