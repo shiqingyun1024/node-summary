d624fd38993f86509a80091db388faa0c1f0efb2
814657780 <18037818812@163.com>
2021-03-28 22:24:12 +0800

    “Node.js”

---
+++ node-basics/04-buildin-modules/03-http/01-get/server.js
@@ -0,0 +1,15 @@
+// 打印日志
+const logger = require('../../utils/log')
+const http = require('http')
+
+const server = http.createServer((request,response)=>{
+    logger.debug(request)
+})
+server.listen(8080,()=>{
+    console.log('localhost:8080');
+})
+
+// 调试命令，在浏览器上会有一个调试页面
+// node --inspect server.js
+// 直接打开浏览器端的调试页面
+// node --inspect  --inspect-brk server.js
