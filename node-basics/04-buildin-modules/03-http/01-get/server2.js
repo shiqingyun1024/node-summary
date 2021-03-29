fbc1f0fc0b2becda65cbf7d09a4996d5cba6d504
814657780 <18037818812@163.com>
2021-03-29 22:39:50 +0800

    “Node.js”

---
+++ node-basics/04-buildin-modules/03-http/01-get/server2.js
@@ -0,0 +1,27 @@
+// 打印日志
+const logger = require('../../utils/log')
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+    let data = "";
+    request.on('data', () => {
+        data += chunk;
+    })
+    request.on('end', () => {
+        // 响应头部的编写，第一个参数是状态码，第二个是content-type,默认是text/html
+        response.writeHead(200, {
+            'content-type': 'text/plain'
+        })
+        response.write('<div>home</div>');
+        response.end();
+    })
+
+})
+server.listen(8080, () => {
+    console.log('localhost:8080');
+})
+
+// 调试命令，在浏览器上会有一个调试页面
+// node --inspect server.js
+// 直接打开浏览器端的调试页面
+// node --inspect  --inspect-brk server.js
