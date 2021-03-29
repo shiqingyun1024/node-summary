f5d22060c523d925e093c7ccd7d4f36842c79338
814657780 <18037818812@163.com>
2021-03-29 21:55:09 +0800

    “Node.js”

--- node-basics/04-buildin-modules/03-http/01-get/server.js
+++ node-basics/04-buildin-modules/03-http/01-get/server.js
@@ -3,7 +3,10 @@ const logger = require('../../utils/log')
 const http = require('http')
 
 const server = http.createServer((request,response)=>{
-    logger.debug(request)
+    // logger.debug(request)
+    // logger.debug(response)
+    response.write('home');
+    response.end();
 })
 server.listen(8080,()=>{
     console.log('localhost:8080');
