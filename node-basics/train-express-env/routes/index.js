51a4cf337318cc982df180180c62520a990b241d
814657780 <18037818812@163.com>
2021-03-30 21:50:26 +0800

    “Node.js”

--- node-basics/train-express-env/routes/index.js
+++ node-basics/train-express-env/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var logger = require('../utils/log')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/data',(req,res,next)=>{
+  logger.debug(req.body);
+})
 
 module.exports = router;
