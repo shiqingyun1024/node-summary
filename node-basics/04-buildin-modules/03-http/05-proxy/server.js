9682c9b171d342c4ba2ac120a699da3ffb461eaa
814657780 <18037818812@163.com>
2021-04-02 23:07:48 +0800

    “Node.js”

--- node-basics/04-buildin-modules/03-http/05-proxy/server.js
+++ node-basics/04-buildin-modules/03-http/05-proxy/server.js
@@ -5,6 +5,15 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 // console.log(proxy);
 
 const server = http.createServer((req,res)=>{
+    let urlstr = req.url
+    if(/\/ajax/.test(urlstr)){
+        const proxy = createProxyMiddleware('/ajax',{
+            target:'https://lady.vip.com',
+            changeOrigin:true
+        })
+    }else{
+        console.log('error');
+    }
 
 })
 
