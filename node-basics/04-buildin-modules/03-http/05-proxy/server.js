ac80217e8b86b3657018a760e111a568883df91a
814657780 <18037818812@163.com>
2021-04-02 23:27:24 +0800

    “Node.js”

--- node-basics/04-buildin-modules/03-http/05-proxy/server.js
+++ node-basics/04-buildin-modules/03-http/05-proxy/server.js
@@ -6,11 +6,14 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 const server = http.createServer((req,res)=>{
     let urlstr = req.url
-    if(/\/ajax/.test(urlstr)){
-        const proxy = createProxyMiddleware('/ajax',{
-            target:'https://lady.vip.com',
+    if(/\/api/.test(urlstr)){
+        const proxy = createProxyMiddleware('/api',{
+            target:'https://px.effirst.com',
             changeOrigin:true
         })
+        proxy(req,res)
+        // 前端请求的是 https://localhost:8080/api/...
+        // 后端进行代理设置，切换源。后端请求的地址是：https://mar.vip.com/api/...
     }else{
         console.log('error');
     }
