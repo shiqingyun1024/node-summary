a3661618dfd2299792e4a87711b63bcd25531523
814657780 <18037818812@163.com>
2021-04-03 12:35:23 +0800

    “Node.js”

---
+++ node-basics/04-buildin-modules/03-http/06-spider/server.js
@@ -0,0 +1,28 @@
+// 蜘蛛 爬虫
+const http = require('http')
+const https = require('https')
+const cheerio = require('cheerio')
+const url = require('url')
+
+function filterData(data) {
+    const $ = cheerio.load(data);
+    console.log(data);
+}
+
+const server = http.createServer((req, res) => {
+    let data = ''
+    https.get('https://www.meizu.com', (result) => {
+        result.on('data', (chunk) => {
+            data += chunk
+        })
+        result.on('end', () => {
+            filterData(data)
+        })
+    })
+
+})
+server.listen(8080, () => {
+    console.log('localhost:8080');
+})
+
+
