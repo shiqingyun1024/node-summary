42a045969acdb07b5617854fd7767554eead4d27
814657780 <18037818812@163.com>
2021-04-04 20:35:05 +0800

    “Node.js”

---
+++ node-basics/08-readline/app.js
@@ -0,0 +1,12 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+rl.question('你如何看待Node.js中文网',(answer)=>{
+    // TODO 将答案记录在数据库中
+    console.log(`感谢您的宝贵意见：${answer}`);
+    rl.close();
+})
