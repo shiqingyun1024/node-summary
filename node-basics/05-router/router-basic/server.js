32fc6ddea841ca288a57bea2cdb28863c38cfb5c
814657780 <18037818812@163.com>
2021-04-04 23:19:54 +0800

    “Node.js”

--- node-basics/05-router/router-basic/server.js
+++ node-basics/05-router/router-basic/server.js
@@ -1,32 +1,45 @@
 const http = require('http')
 const fs = require('fs')
+const mime = require('mime')
 
-http.createServer((req, res) => {
+http
+// .createServer((req, res) => {
+//     const urlString = req.url
+//     console.log(urlString);
+//     switch (urlString) {
+//         case '/':
+//             res.end('hello')
+//             break
+//         case '/home':
+//             fs.readFile('./home.html', (err, content) => {
+//                 res.end(content)
+//             })
+//             break
+//         case '/app.js':
+//             fs.readFile('./app.js', (err, content) => {
+//                 res.end(content)
+//             })
+//             break
+//         case '/summary.jpg': // binary是二进制的意思
+//                 fs.readFile('./summary.jpg', (err, content) => {
+//                     res.end(content)
+//                 })
+//                 break    
+//         default:
+//             res.end('page：404')
+//     }
+
+// })
+// 上面代码更简洁的写法
+.createServer((req,res)=>{
     const urlString = req.url
-    console.log(urlString);
-    switch (urlString) {
-        case '/':
-            res.end('hello')
-            break
-        case '/home':
-            fs.readFile('./home.html', (err, content) => {
-                res.end(content)
-            })
-            break
-        case '/app.js':
-            fs.readFile('./app.js', (err, content) => {
-                res.end(content)
-            })
-            break
-        case '/summary.jpg': // binary是二进制的意思
-                fs.readFile('./summary.jpg', (err, content) => {
-                    res.end(content)
-                })
-                break    
-        default:
-            res.end('page：404')
-    }
-
-}).listen(8080, () => {
+    const type = mime.getType(urlString.split('.')[1])
+    res.writeHead(200,{
+        'content-type':type
+    })
+    const file = fs.readFileSync(`.${urlString}`)
+    res.end(file)
+})
+.listen(8080, () => {
     console.log('localhost:8080');
 })
