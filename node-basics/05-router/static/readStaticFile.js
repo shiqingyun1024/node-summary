5e3d19e95ede4b2d8386112752204c8d7195d9ef
814657780 <18037818812@163.com>
2021-04-05 21:48:00 +0800

    “Node.js”

--- node-basics/05-router/static/readStaticFile.js
+++ node-basics/05-router/static/readStaticFile.js
@@ -1,10 +1,18 @@
 const path = require('path');
 // mime根据扩展名来生成对应的类型,mime.getType()
 const mime = require('mime');
+const fs = require('fs')
 
 function readStaticFile(filePathName){
     let ext = mime.getType(path.parse(filePathName).ext)
     console.log(ext);
+
+    // 判断文件是否存在
+    if(fs.existsSync(filePathName)){
+        console.log('文件存在');
+    }else{
+        console.log('文件不存在');
+    }
 
     return 'hello'
 }
