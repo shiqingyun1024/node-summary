1aa842b0c781f8abb34eb4dba0a157c89df28621
814657780 <18037818812@163.com>
2021-04-07 22:41:00 +0800

    “Node.js”

--- express-basics/server.js
+++ express-basics/server.js
@@ -4,7 +4,11 @@ const app = express()
 
 const router = require('./router/index')
 
+const bodyParser = require('body-parser')
+
 app.use('/',router)
+
+app.use(bodyParser.urlencoded({extend:false}))
 
 app.listen(8080,()=>{
     console.log('localhost:8080');
