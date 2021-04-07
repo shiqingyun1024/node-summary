d0ead1d315c038840c9d6ee7fec93ac69d7fcf98
814657780 <18037818812@163.com>
2021-04-07 21:56:38 +0800

    “Node.js”

--- express-basics/server.js
+++ express-basics/server.js
@@ -1,29 +1,6 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-let middlewares = [(req,res,next)=>{
-    console.log(0);
-    next()
-},(req,res,next)=>{
-    console.log(1);
-    next()
-},(req,res,next)=>{
-    console.log(2);
-    next()
-}]
-
-// 回调函数又被称为中间件
-// 中间件栈
-app.use('/',middlewares)
-
-app.use('/ajax',(req,res,next)=>{
-    console.log('ajax');
-    // next()
-})
-
-app.use('/api',(req,res)=>{
-    res.end('world')
-})
+const app = express()
 
 app.listen(8080,()=>{
     console.log('localhost:8080');
