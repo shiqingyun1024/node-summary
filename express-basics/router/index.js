0be83e673e1683f4ace282ee56590867e517e9ae
814657780 <18037818812@163.com>
2021-04-07 22:13:13 +0800

    “Node.js”

--- express-basics/router/index.js
+++ express-basics/router/index.js
@@ -1,1 +1,14 @@
 const express = require('express')
+
+// 路由中间件
+const router = express.Router()
+
+router.get('/',(req,res,next)=>{
+    res.send('hello')
+})
+
+router.get('/index',(req,res,next)=>{
+    res.send('index pages')
+})
+
+module.exports = router
--- express-basics/server.js
+++ express-basics/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+const router = require('./router/index')
+
+app.use('/',router)
+
 app.listen(8080,()=>{
     console.log('localhost:8080');
 })
