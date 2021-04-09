0910683f73473a180f389caa4e3d3100cf556926
814657780 <18037818812@163.com>
2021-04-09 22:38:45 +0800

    “Node.js”

--- express-basics/public/scripts/common.js
+++ express-basics/public/scripts/common.js
@@ -1,1 +1,12 @@
-console.log('123');
+$.ajax({
+    url:'/api/list',
+    success(result){
+        console.log(result);
+        let html = '<ul>'
+        $.each(result.data,(index,value)=>{
+            html+='<li>'+value+'</li>'
+        })
+        html +="</ul>"
+        $('#list').html(html)
+    }
+})
