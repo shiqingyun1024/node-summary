3f163d58264762e8462de505e4d5eecb2d6012aa
814657780 <18037818812@163.com>
2021-04-09 21:50:45 +0800

    “Node.js”

--- express-basics/server.js
+++ express-basics/server.js
@@ -12,6 +12,9 @@ app.use(bodyParser.urlencoded({extend:false}))
 // parse application/json    解析json字符串
 app.use(bodyParser.json())
 
+// 静态资源服务中间件（内置中间件）
+app.use(express.static('public'))
+
 
 app.use('/',router)
 
