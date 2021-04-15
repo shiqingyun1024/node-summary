32a5be6fe3d69930f099ee38d606f952ed115808
814657780 <18037818812@163.com>
2021-04-16 00:12:35 +0800

    “Node.js”

--- node-basics/06-socket/websocket/SocketServer.js
+++ node-basics/06-socket/websocket/SocketServer.js
@@ -1,13 +1,24 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({
+    port: 8080
+});
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
+    ws.on('open', function open() {
+        console.log('connected');
+        ws.send(Date.now());
     });
-  });
+
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    });
+});
+
+wss.on('close', function close() {
+    console.log('disconnected');
 });
