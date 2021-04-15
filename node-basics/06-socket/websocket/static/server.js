d546d366a404a8faf3328347bdb37999e6be1047
814657780 <18037818812@163.com>
2021-04-15 23:44:05 +0800

    “Node.js”

---
+++ node-basics/06-socket/websocket/static/server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const path = require('path')
+const readStaticFile = require('./readStaticFile')
+http.createServer(async (req,res)=>{
+    let urlstring = req.url;
+    let filePathName = path.join(__dirname,'./public',urlstring)
+    let {data,mimeType } = await readStaticFile(filePathName)
+
+    console.log(mimeType);
+    console.log(1);
+    console.log(data.toString());
+    res.writeHead(200,{
+        'content-type':`${mimeType};charset=utf-8`
+    })
+    res.write(data)
+    res.end()
+}).listen(8080,()=>{
+    console.log('localhost:8080');
+})
