94e23bc2b67a6698c3f8aed4f0a828a288125912
814657780 <18037818812@163.com>
2021-04-17 21:48:57 +0800

    “Node.js”

--- laogou-admin/frontend/webpack.config.js
+++ laogou-admin/frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const {resolve} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 // webpack是基于node.js的，所以遵循commonjs规范
 module.exports = {
     // 入口文件
@@ -11,6 +12,14 @@ module.exports = {
         filename:'app.js',
         path:resolve(__dirname,'dist')
     },
+
+    // 插件plugin的集合
+    plugins:[
+        new HtmlWebpackPlugin({
+
+        })
+    ]
+
 
     // 模式
     mode: 'development'
