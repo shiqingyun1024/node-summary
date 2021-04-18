a2ebb512b12e437bfa19d38f361b5d4268dfed99
814657780 <18037818812@163.com>
2021-04-18 13:45:59 +0800

    安装art-template art-template-loader

--- laogou-admin/frontend/webpack.config.js
+++ laogou-admin/frontend/webpack.config.js
@@ -19,11 +19,18 @@ module.exports = {
         path: resolve(__dirname, 'dist')
     },
 
+    // loader的结合
+    module: {
+        rules: [
+
+        ]
+    },
+
     // 插件plugin的集合
     plugins: [
         // 把打包后的资源引入html中，如果HtmlWebpackPlugin中不写template这个参数，HtmlWebpackPlugin会自己创建一个index.html,然后引入打包之后的文件
         new HtmlWebpackPlugin({
-            template:'./public/index.html'
+            template: './public/index.html'
         }),
         // copy文件到dist目录下
         // new CopyWebpackPlugin({patterns:[{ from: 'public/favicon.ico', to: 'imgages' }]}),  // CopyWebpackPlugin6.0以上的版本这样使用
@@ -34,7 +41,7 @@ module.exports = {
         ]),
         // 打包的时候每次先清空dist目录
         new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns:['dist']
+            cleanAfterEveryBuildPatterns: ['dist']
         })
     ],
 
