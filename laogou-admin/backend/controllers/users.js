86e1671b1a37b934d1dfd170ec9aef1064e0546b
814657780 <18037818812@163.com>
2021-04-20 23:23:36 +0800

    判断用户名是否存在,并做相应的处理

--- laogou-admin/backend/controllers/users.js
+++ laogou-admin/backend/controllers/users.js
@@ -1,23 +1,33 @@
 const usersModel = require('../models/users')
 
 // 注册用户
-const signup = async (req,res,next)=>{
-    const {username,password} = req.body
+const signup = async (req, res, next) => {
+    const { username, password } = req.body
 
     // 判断用户是否存在
     let findResult = await usersModel.findUser(username)
     console.log(findResult);
-    // let result = await usersModel.signup({
-    //     username,
-    //     password
-    // })
-    // console.log(result);
-    // res.render('succ',{
-    //     data:JSON.stringify({
-    //         username,
-    //         password
-    //     })
-    // })
+    if (findResult) {  // 用户存在
+      res.render('fail',{
+          data:JSON.stringify({
+            message:'用户名已存在。'
+        })
+      })
+    } else {// 用户不存在
+        // 数据库里没有这个用户，开始添加用户
+        let result = await usersModel.signup({
+            username,
+            password
+        })
+        console.log(result);
+        res.render('succ', {
+            data: JSON.stringify({
+                username,
+                password
+            })
+        })
+    }
+
 }
 
 module.exports = signup
