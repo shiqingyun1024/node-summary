23f3bf60af0048c3694c1c09008e17dde9493bb3
814657780 <18037818812@163.com>
2021-04-23 23:46:47 +0800

    前端增加删除用户的操作

--- laogou-admin/backend/controllers/users.js
+++ laogou-admin/backend/controllers/users.js
@@ -1,32 +1,37 @@
 const usersModel = require('../models/users')
-const { hash } = require('../utils/tools')
+const {
+    hash
+} = require('../utils/tools')
 
 // 注册用户
 const signup = async (req, res, next) => {
-    res.set('content-type','application/json;charset=utf-8')
-    const { username, password } = req.body
-    
+    res.set('content-type', 'application/json;charset=utf-8')
+    const {
+        username,
+        password
+    } = req.body
+
     let bcryptPassword = await hash(password);
 
     // 判断用户是否存在
     let findResult = await usersModel.findUser(username)
     console.log(findResult);
-    if (findResult) {  // 用户存在
-      res.render('fail',{
-          data:JSON.stringify({
-            message:'用户名已存在。'
+    if (findResult) { // 用户存在
+        res.render('fail', {
+            data: JSON.stringify({
+                message: '用户名已存在。'
+            })
         })
-      })
-    } else {// 用户不存在
+    } else { // 用户不存在
         // 数据库里没有这个用户，开始添加用户
         let result = await usersModel.signup({
             username,
-            password:bcryptPassword
+            password: bcryptPassword
         })
-        
+
         res.render('succ', {
             data: JSON.stringify({
-                message:'注册成功！'
+                message: '注册成功！'
             })
         })
     }
@@ -34,19 +39,38 @@ const signup = async (req, res, next) => {
 }
 
 // 用户列表
-const list = async (req,res,next)=>{
-    res.set('content-type','application/json;charset=utf-8')
+const list = async (req, res, next) => {
+    res.set('content-type', 'application/json;charset=utf-8')
     const listResult = await usersModel.findList()
-    res.render('succ',{
-      data:JSON.stringify(listResult)   
+    res.render('succ', {
+        data: JSON.stringify(listResult)
     })
 }
 
 // 删除用户
-const remove = async (req,res,next)=>{
-   let {id} = req.body
-  // 记住一定要加await   
-   await usersModel.remove(id)
+const remove = async (req, res, next) => {
+    res.set('content-type', 'application/json;charset=utf-8')
+    let {id} = req.body
+    // 记住一定要加await   
+    let result = await usersModel.remove(id)
+    if (result) {
+        res.render('succ', {
+            data: JSON.stringify({
+                message: '用户删除成功'
+            })
+        })
+    }
+    res.render('fail', {
+        data: JSON.stringify({
+            message: '用户删除失败'
+        })
+    })
+
+
 }
 
-module.exports = {signup,list,remove}
+module.exports = {
+    signup,
+    list,
+    remove
+}
