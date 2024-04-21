d6f5e2605999b9f122ba91edb3d822b23416b1cc
814657780 <18037818812@163.com>
2024-04-21 21:55:00 +0800

    node.js的学习

--- node的学习/http/01、创建http服务.js
+++ node的学习/http/01、创建http服务.js
@@ -3,7 +3,10 @@ const http = require('http')
 
 // 2、创建服务对象
 const server = http.createServer((request, response) => {
-    response.end('Hello HTTP Server'); // 设置响应体
+    // response.end('Hello HTTP Server'); // 设置响应体
+    // 如果直接返回中文会乱码，所以需要先设置请求头内容
+    response.setHeader('content-type', 'text/html;charset=utf-8')
+    response.end('你好');
 })
 
 // 3、监听端口，启动服务
