3e3f1baee3bf11f30bfead5b6974b2c2de0694c4
814657780 <18037818812@163.com>
2024-04-22 23:43:08 +0800

    node.js的学习

--- node的学习/http/07、HTTP请求练习.js
+++ node的学习/http/07、HTTP请求练习.js
@@ -1,16 +1,21 @@
-// 导入http模块
+// 1、导入http模块
 const http = require('http')
 
-// 1、导入url模块
-const url = require('url')
-
-// 创建服务对象
+// 2、创建服务对象
 const server = http.createServer((request, response) => {
-    // 2、解析request.url
-    // console.log(request.url)
-    let res = url.parse(request.url);
-    console.log(res);
-    response.end('url');
+    // 获取请求的方法
+    // let method = request.method
+    let { method } = request; // 使用解构赋值
+    // 获取请求的url路径
+    let { pathname } = new URL(request.url, 'http://127.0.0.1')
+    response.setHeader('content-type', 'text/html;charset=utf-8')
+    if (method === 'GET' && pathname === '/login') {
+        response.end('登录页面');
+    } else if (method === 'GET' && pathname === '/reg') {
+        response.end('注册页面');
+    } else {
+        response.end('Not Found');
+    }
 })
 
 // 3、监听端口，启动服务
