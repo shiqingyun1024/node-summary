675f9b41305e15c5623f7018e90acf639f974cbf
814657780 <18037818812@163.com>
2024-04-29 23:16:23 +0800

    node.js的学习

--- node的学习/http/13、导入（引入）模块/show.js
+++ node的学习/http/13、导入（引入）模块/show.js
@@ -9,7 +9,21 @@ function require(file) {
     //3.读取文件的代码
     let code = fs.readFileSync(absolutePath).toString();
     //4.包裹为一个函数 然后执行
+    let module = {}
+    let exports = module.exports = {}
+        (function (exports, require, module, __filename, __dirname) {
+            const test = {
+                name: '尚硅谷',
+            }
+            module.exports = test;
+            //输出
+            console.log(arguments.callee.toString());
+        })(exports, require, module, __filename, __dirname)
 
+    // 5、缓存模块的值
+    caches[absolutePath] = module.exports
+    // 6、返回 module.exports 的值
+    return module.exports
 }
 
 const m = require('./me.js')
