63f312af1f514df0b30bf217e0048882eb7130e0
814657780 <18037818812@163.com>
2024-05-07 23:17:37 +0800

    express框架的学习

--- node的学习/express/08.全局中间件.js
+++ node的学习/express/08.全局中间件.js
@@ -1,11 +1,30 @@
 // 导入express
 const express = require('express')
+const fs = require('fs');
+const path = require('path');
 
 // 创建应用对象
 const app = express();
 
+// 声明中间件函数
+function recordMiddleware(req, res, next) {
+    // 获取 url 和 ip
+    let { url, ip } = req;
+    // 将信息保存在文件中 access.log
+    fs.appendFileSync(path.resolve(__dirname, './access.log'), `${url}  ${ip}\r\n`)
+    // 调用next
+    next()
+}
+
+// 使用中间件函数
+app.use(recordMiddleware)
+
 // 创建路由
 app.get('/home', (req, res) => {
+    // // 获取url和ip
+    // let { url, ip } = req;
+    // // 将信息保存在文件中 access.log
+    // fs.appendFileSync(path.resolve(__dirname, './access.log'), `${url}  ${ip}\r\n`)
     res.send('前台首页')
 })
 app.get('/admin', (req, res) => {
