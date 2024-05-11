41eed3475dd4817d0c31426e8fdaa40ac8c63edd
814657780 <18037818812@163.com>
2024-05-12 00:07:11 +0800

    文件上传 formidable

--- node的学习/express/15.generator/routes/index.js
+++ node的学习/express/15.generator/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const formidable = require('formidable');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// 显示网页的（表单）
+router.get('/portrait', (req, res) => {
+  res.render('portrait');
+});
+
+// 处理文件上传
+router.post('/portrait', (req, res) => {
+  const form = formidable({});
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json({ fields, files });
+  });
+})
 
 module.exports = router;
