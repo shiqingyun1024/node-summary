b5e9b31865a299b7c6ec5b71b43f715acbec0e73
814657780 <18037818812@163.com>
2024-05-12 06:40:23 +0800

    express的案例

--- node的学习/express/15.generator/routes/index.js
+++ node的学习/express/15.generator/routes/index.js
@@ -14,8 +14,14 @@ router.get('/portrait', (req, res) => {
 
 // 处理文件上传
 router.post('/portrait', (req, res) => {
+  // res.send("成功")
   // 创建form对象
-  const form = formidable({});
+  const form = formidable({
+    multiples: true,
+    // 设置上传文件的保存目录
+    uploadDir: __dirname + '/../public/images'
+    // 保持文件后缀
+  });
 
   // 解析请求报文
   form.parse(req, (err, fields, files) => {
@@ -25,7 +31,11 @@ router.post('/portrait', (req, res) => {
     }
     console.log(fields); // text radio checkbox select
     console.log(files); // file
-    res.send('OK')
+
+    // 服务器保存该图片的访问 URL
+    // 
+    let url = "/images/" + files.portrait.newFilename; // 将来将此数据保存在数据库中
+    res.send(url)
     // res.json({ fields, files });
   });
 })
