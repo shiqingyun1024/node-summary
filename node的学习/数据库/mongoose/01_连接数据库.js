6c28f94067d85c489bfae2c08f0e11e190f4912d
814657780 <18037818812@163.com>
2024-05-16 06:06:13 +0800

    express和ejs模板案例

---
+++ node的学习/数据库/mongoose/01_连接数据库.js
@@ -0,0 +1,19 @@
+// 1、安装mongoose
+// 2、导入mongoose
+const mongoose = require('mongoose');
+
+// 3、链接mongodb服务                   bilibili是数据库的名称  27017是mongodb的默认端口号
+mongoose.connect('mongodb://127.0.0.1:27017/bilibili');
+
+//4.设置回调
+mongoose.connection.on('open', () => {
+    console.log('连接成功')
+}); // 设置连接成功的回调
+
+mongoose.connection.on('error', () => {
+    console.log('连接失败');
+})// 设置连接错误的回调
+
+mongoose.connection.on('close', () => {
+    console.log('连接关闭');
+})// 设置连接关闭的回调
