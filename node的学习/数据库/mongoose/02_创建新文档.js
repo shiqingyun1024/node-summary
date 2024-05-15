925848f2e0adc5bd76d68fb0d44a5c9edd89d407
814657780 <18037818812@163.com>
2024-05-16 06:38:03 +0800

    mongodb和mongoose

--- node的学习/数据库/mongoose/02_创建新文档.js
+++ node的学习/数据库/mongoose/02_创建新文档.js
@@ -6,19 +6,9 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/bilibili');
 
 //4.设置回调
-// 设置连接成功的回调
-mongoose.connection.on('open', () => {
-    console.log('连接成功')
-});
-
-// 设置连接错误的回调
-mongoose.connection.on('error', () => {
-    console.log('连接失败');
-})
-
-// 设置连接关闭的回调   once 一次  事件回调只执行一次
-mongoose.connection.once('close', () => {
-    console.log('连接关闭');
+// 设置连接成功的回调   once 一次  事件回调只执行一次
+mongoose.connection.once('open', () => {
+    console.log('连接成功');
     //5. 创建文档的结构对象
     //设置集合中文档的属性以及属性值的类型
     let BookSchema = new mongoose.Schema({
@@ -33,9 +23,9 @@ mongoose.connection.once('close', () => {
         name: '西游记',
         author: '吴承恩',
         price: 19.9
-    }, (err, data) = {
+    }, (err, data) => {
         //判断是否有错误
-        if(err) {
+        if (err) {
             console.log(err);
             return;
         }
@@ -44,6 +34,18 @@ mongoose.connection.once('close', () => {
         // console.log(data);
     });
 })
+
+
+// 设置连接错误的回调
+mongoose.connection.on('error', () => {
+    console.log('连接失败');
+})
+
+// 设置连接关闭的回调
+mongoose.connection.on('close', () => {
+    console.log('连接关闭')
+});
+
 
 // 关闭mongoose的链接
 // setTimeout(() => {
